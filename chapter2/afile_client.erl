babbfa017be1ab0cbb429d93ec7a6377927809ef
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2017-05-14 19:30:19 +0300

    Add put_file.

--- chapter2/afile_client.erl
+++ chapter2/afile_client.erl
@@ -1,5 +1,5 @@
 -module(afile_client).
--export([ls/1, get_file/2]).
+-export([ls/1, get_file/2, put_file/3]).
 
 ls(Server) ->
     Server ! {self(), list_dir},
@@ -13,4 +13,11 @@ get_file(Server, File) ->
     receive
         {Server, Content} ->
             Content
+    end.
+
+put_file(Server, File, Content) ->
+    Server ! {self(), {put_file, File, Content}},
+    receive
+        {Server, Result} ->
+            Result
     end.
